08529650e79335b19e00f58f126cf37c8bd4cf05
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-01-29 22:29:10 -0500

    Added Firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Firebase
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB25gxYu7PkMvnjKhisCzPP62grU_OEMLU",
+  authDomain: "solutions2021-efce9.firebaseapp.com",
+  projectId: "solutions2021-efce9",
+  storageBucket: "solutions2021-efce9.appspot.com",
+  messagingSenderId: "1033043841752",
+  appId: "1:1033043841752:web:d1f78f6778f22f4d423dad"
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +27,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    //firebase
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
