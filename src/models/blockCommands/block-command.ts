8e02ed24b4c2c59612884398a49f9767a218996c
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-02-14 22:09:19 -0500

    Added Base Block Command

---
+++ src/models/blockCommands/block-command.ts
@@ -0,0 +1,40 @@
+import { GameAction } from "../game/GameAction";
+
+
+export interface BlockCommand{
+
+  getId: () => number
+  getLabel: () => string
+  indentationLevel: number
+  getAsCode: () => string
+
+}
+
+export interface Predicate extends BlockCommand{
+
+  evaluation: (grid, unit) => boolean
+
+}
+
+export interface Executable extends BlockCommand{
+
+  execute: (grid, unit) => GameAction
+
+}
+
+export interface ConditionalBlock extends BlockCommand{
+
+  condition: Predicate;
+  terminal_blocks: Array<string>;
+
+}
+
+export interface TerminalBlock extends BlockCommand{
+
+  /*
+   This is just to check if a BlockCommand is a terminal block by checking if this is not undefined. Otherwise,
+   it has not significance
+   */
+  terminate: number;
+
+}
