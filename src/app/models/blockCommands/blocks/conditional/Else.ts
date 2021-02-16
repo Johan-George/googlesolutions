74f3f9db9838a51eaf1dc4f1f2fcfd09ae9b9b5a
john-amiscaray <john_ernest_amiscaray@outlook.com>
2021-02-15 21:20:22 -0500

    fixed the bugs accidentally introduced when merging (thanks kyle)

--- src/app/models/blockCommands/blocks/conditional/Else.ts
+++ src/app/models/blockCommands/blocks/conditional/Else.ts
@@ -1,20 +1,22 @@
-import { ConditionalBlock, Predicate } from '../../block-command';
+import {ConditionalBlock, Predicate, TerminalBlock} from '../../block-command';
 import { EndElse } from '../terminal/EndElse';
+import {EmptyPredicate} from '../predicate/EmptyPredicate';
 
 /**
  * ConditionalBlock representing an Else statement
- * See block-command.ts for specific documentation 
+ * See block-command.ts for specific documentation
  * on properties and methods
  */
-export class Else implements ConditionalBlock {
+export class Else implements ConditionalBlock, TerminalBlock {
 
   static id: string = btoa(Else.name);
   static label: string = 'Else';
   static asCode: string = '} else {';
 
-  condition: Predicate;
+  condition: Predicate = new EmptyPredicate();
   terminal_blocks: Array<string> = [EndElse.label];
   indentationLevel: number;
+  terminate: number = null;
 
   getId(): string {
     return Else.id;
