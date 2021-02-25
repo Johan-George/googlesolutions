8886bb1ae54dbcd9ae86b99f34c19a54a4a85006
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-02-24 21:31:24 -0500

    Added Signin Page Components FINALLY

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {BlockCodeComponent} from './components/block-code/block-code.component';
 import { TestComponent } from './components/test/test.component';
 import {LevelComponent} from './components/level/level.component';
+import { SigninPageComponent } from './components/signin-page/signin-page.component';
 
 const routes: Routes = [
   {path: 'code', component: BlockCodeComponent},
   {path: 'debug', component:TestComponent},
-  {path: 'level', component: LevelComponent}
+  {path: 'level', component: LevelComponent},
+  {path: 'signin', component: SigninPageComponent}
 ];
 
 @NgModule({
